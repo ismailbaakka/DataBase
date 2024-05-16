947727bee855163232b152a260538e1e2519b3f1
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-05-16 03:23:11 +0100

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -14,16 +14,18 @@
         blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b12073');
         blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b59147');
         blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
-        blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
         blockElementsByClass('fqlverki');
         blockElementsByClass('WorkTeam');
         blockElementsByClass('ps_ad_rotation_id_5446');
         blockElementsByClass('div-over');
-        blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
         blockElementsByClass('x3z10fv');
         blockElementsByClass('ad_position_box');
         blockElementsByClass('ps_ad_rotation_id_5446');
         blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b63467');
+        blockElementsByClass('ps_ad_rotation_id_5199');
+        blockElementsByClass('div-gpt-ad-1714577946689-0');
+        blockElementsByClass('MainMenuOverlay');
+        blockElementsByClass('gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gamestation_0');
         blockElementsByClass('');
         blockElementsByClass('');
         blockElementsByClass('');
@@ -35,11 +37,7 @@
         blockElementsByClass('');
         blockElementsByClass('');
         blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        
+
 
 
 
@@ -54,7 +52,8 @@
             'google_ads_iframe_/22654869840/apl_1__container__',
             'google_ads_iframe_/22893379435/apl_0__container__',
             'dontfoid', // <-- Remove this element
-            'gpt_unit_/7047,22643491855/apl/anchor/anchortop_0'
+            'gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gamestation_0',
+            'l7qz7g1'
         ];
 
         elementsToRemoveById.forEach(function(elementId) {
