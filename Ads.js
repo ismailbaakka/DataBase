a8571ccf5f079887d68b6e59630ae267f549bfb7
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-05-16 05:04:23 +0100

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -1,81 +1,62 @@
- function blockElementsByClass(className) {
-            var elements = document.getElementsByClassName(className);
+function blockElementsByClassStartsWith(startStr) {
+    var allElements = document.getElementsByTagName('*');
 
-            // Remove elements from the DOM
-            while (elements.length > 0) {
-                elements[0].parentNode.removeChild(elements[0]);
+    for (var i = 0; i < allElements.length; i++) {
+        var classes = allElements[i].className.split(' ');
+        for (var j = 0; j < classes.length; j++) {
+            if (classes[j].startsWith(startStr)) {
+                allElements[i].parentNode.removeChild(allElements[i]);
             }
         }
+    }
+}
 
+blockElementsByClassStartsWith('container-6f4f5c3f5bfa5f5651799c658cb3556b');
 
-        blockElementsByClass('PageHeader');
-        blockElementsByClass('SlideMenu');
-        blockElementsByClass('relatedPosts');
-        blockElementsByClass('row');
-        blockElementsByClass('AboElSeed');
-        blockElementsByClass('ads-aa');
-        blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b40743');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b6839');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b49835');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b13062');
-        blockElementsByClass('gtop show');
-        blockElementsByClass('GoogleCreativeContainerClass');
-        blockElementsByClass('over-close');
+function blockElementsByClass(className) {
+    var elements = document.getElementsByClassName(className);
 
-        
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b32535');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b62840');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b9969');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b8124');
+    // Remove elements from the DOM
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 
+blockElementsByClass('PageHeader');
+blockElementsByClass('SlideMenu');
+blockElementsByClass('relatedPosts');
+blockElementsByClass('row');
+blockElementsByClass('AboElSeed');
+blockElementsByClass('ads-aa');
+blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
+blockElementsByClass('gtop show');
+blockElementsByClass('GoogleCreativeContainerClass');
+blockElementsByClass('over-close');
 
-        
+// Remove elements by ID
+var elementsToRemoveById = [
+    'div-gpt-ad-1714577946689-0',
+    'div-gpt-ad-1715073293748-0',
+    'ps_ad_rotation_id_5446',
+    'ad_unit',
+    'fixedban2',
+    'fixedban',
+    'google_ads_iframe_/21727820151,22953604694/Gametech_HM/video-ad2_0__container__',
+    'container-6f4f5c3f5bfa5f5651799c658cb3556b44923',
+    'div-gpt-ad-1714577932847-0',
+    'google_ads_iframe_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0__container__',
+    'gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0'
+];
 
+elementsToRemoveById.forEach(function(elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.remove();
+    }
+});
 
-
-
-
-        //End Page 1
-        blockElementsByClass('MainMenuOverlay');
-        blockElementsByClass('mys-wrapper');
-        blockElementsByClass('GoogleActiveViewInnerContainer');
-        blockElementsByClass('div-over');
-        blockElementsByClass('WatcherArea');
-
-
-
-
-
-
-
-
-
-        // Remove elements by ID
-        var elementsToRemoveById = [
-            'div-gpt-ad-1714577946689-0',
-            'div-gpt-ad-1715073293748-0',
-            'ps_ad_rotation_id_5446',
-            'ad_unit',
-            'fixedban2',
-            'fixedban',
-            'google_ads_iframe_/21727820151,22953604694/Gametech_HM/video-ad2_0__container__',
-            'container-6f4f5c3f5bfa5f5651799c658cb3556b44923',
-            'div-gpt-ad-1714577932847-0',
-            'google_ads_iframe_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0__container__',
-            'gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0'
-
-        ];
-
-        elementsToRemoveById.forEach(function(elementId) {
-            var element = document.getElementById(elementId);
-            if (element) {
-                element.remove();
-            }
-        });
-
-        // Remove additional aplvideo elements by class name
-        var aplvideoDivs = document.getElementsByClassName('aplvideo');
-        while (aplvideoDivs.length > 0) {
-            aplvideoDivs[0].remove();
-        }
+// Remove additional aplvideo elements by class name
+var aplvideoDivs = document.getElementsByClassName('aplvideo');
+while (aplvideoDivs.length > 0) {
+    aplvideoDivs[0].remove();
+}
