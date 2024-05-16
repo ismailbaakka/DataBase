15b00d4bf1a31011c08ae3add522f262e2accd01
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-05-16 01:23:51 +0100

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -8,34 +8,25 @@
         }
 
         blockElementsByClass('PageHeader');
-
         blockElementsByClass('row');
-
         blockElementsByClass('AboElSeed');
-
         blockElementsByClass('relatedPosts');
-
         blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b12073');
-
         blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b59147');
-
         blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
-
         blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
-
         blockElementsByClass('fqlverki');
-
         blockElementsByClass('WorkTeam');
-
         blockElementsByClass('ps_ad_rotation_id_5446');
-
         blockElementsByClass('div-over');
-
         blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
-
-        blockElementsByClass('');
-
-        blockElementsByClass('');
+        blockElementsByClass('x3z10fv');
+        blockElementsByClass('ad_position_box');
+        blockElementsByClass('ps_ad_rotation_id_5446');
+        blockElementsByClass('ivq3325');
+        blockElementsByClass('Adsense');
+        blockElementsByClass('rts1-urts1');
+        blockElementsByClass('t1.gameland.click');
 
 
 
@@ -43,7 +34,7 @@
 
         // Remove elements by ID
         var elementsToRemoveById = [
-            'id-custom_banner',
+            't1.gameland.click',
             'google_ads_iframe_/22654869840/apl_1__container__',
             'google_ads_iframe_/22893379435/apl_0__container__',
             'dontfoid', // <-- Remove this element
