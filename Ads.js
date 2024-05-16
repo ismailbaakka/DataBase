c763a64a8d94d343025f805ed880a9fe589af4fd
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-05-16 04:25:49 +0100

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -53,7 +53,9 @@
             'fixedban',
             'google_ads_iframe_/21727820151,22953604694/Gametech_HM/video-ad2_0__container__',
             'container-6f4f5c3f5bfa5f5651799c658cb3556b44923',
-            'div-gpt-ad-1714577932847-0'
+            'div-gpt-ad-1714577932847-0',
+            'google_ads_iframe_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0__container__',
+            'gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0'
 
         ];
 
