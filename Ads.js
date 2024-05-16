76117f3d89443716497b417d6ca216947c93e61c
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-05-16 03:52:09 +0100

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -7,45 +7,18 @@
             }
         }
 
+
         blockElementsByClass('PageHeader');
+        blockElementsByClass('SlideMenu');
+        blockElementsByClass('relatedPosts');
         blockElementsByClass('row');
         blockElementsByClass('AboElSeed');
-        blockElementsByClass('relatedPosts');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b12073');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b59147');
-        blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
-        blockElementsByClass('fqlverki');
-        blockElementsByClass('WorkTeam');
-        blockElementsByClass('div-over');
-        blockElementsByClass('x3z10fv');
-        blockElementsByClass('ad_position_box');
-        blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b63467');
-        blockElementsByClass('ps_ad_rotation_id_5199');
-        blockElementsByClass('div-gpt-ad-1714577946689-0');
-        blockElementsByClass('MainMenuOverlay');
-        blockElementsByClass('gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gamestation_0');
         blockElementsByClass('');
         blockElementsByClass('');
         blockElementsByClass('');
         blockElementsByClass('');
         blockElementsByClass('');
         blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('');
-        blockElementsByClass('ad-video');
-        blockElementsByClass('fixedban');
-
-        blockElementsByClass('rts1-urts1');
-        blockElementsByClass('div-gpt-ad-1715073293748-0');
-        blockElementsByClass('modal');
-        blockElementsByClass('containerIframe');
-        blockElementsByClass('WatcherArea');
-        blockElementsByClass('gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0');
-        blockElementsByClass('fixedban2');
 
 
 
@@ -57,10 +30,12 @@
 
         // Remove elements by ID
         var elementsToRemoveById = [
-            'google_ads_iframe_/21727820151,22953604694/Gametech_HM/gl3_0__container__',
-            'google_ads_iframe_/21727820151,22953604694/Gametech_HM/video-ad2_0__container__',
-            'ps_ad_rotation_id_5446',
-            'gpt_unit_/21727820151,22953604694/Gametech_HM/video-ad2_0'
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
 
         ];
 
